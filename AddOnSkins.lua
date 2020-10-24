c0390001beefca8d90e817498ca4a2bfdebfa105
Toxicom <69549795+Toxicom@users.noreply.github.com>
2020-10-24 15:14:25 +0300

    Add files via upload
    added ProjectAzilroka and AddOnSkins profiles

---
+++ AddOnSkins.lua
@@ -0,0 +1,34 @@
+
+AddOnSkinsDB = {
+	["profileKeys"] = {
+		["Toxicom - Tarren Mill"] = "Default",
+		["Toxicom - Ravencrest"] = "Default",
+		["Calistrø - Ravencrest"] = "Default",
+		["Toxilock - Tarren Mill"] = "Default",
+		["Twinklebeard - Auchindoun"] = "Default",
+		["Toxicòm - Ravencrest"] = "Default",
+		["Tòxicom - Ravencrest"] = "Default",
+		["Toxicøm - Ravencrest"] = "Default",
+		["Toximoon - Tarren Mill"] = "Default",
+		["Toxiholy - Tarren Mill"] = "Default",
+		["Toxirage - Tarren Mill"] = "Default",
+		["Toxiwiz - Tarren Mill"] = "Default",
+		["Toxiglide - Tarren Mill"] = "Default",
+		["Toxí - Ravencrest"] = "Default",
+		["Calìstro - Ravencrest"] = "Default",
+		["Toxiquiver - Tarren Mill"] = "Default",
+		["Toxilich - Ravencrest"] = "Default",
+		["Toxifurry - Tarren Mill"] = "Default",
+		["Toxisin - Tarren Mill"] = "Default",
+		["Tøxicom - Kazzak"] = "Default",
+		["Melissandei - Ravencrest"] = "Default",
+	},
+	["profiles"] = {
+		["Default"] = {
+			["DBMFont"] = "Gilroy Bold",
+			["AAP-Core"] = false,
+		},
+	},
+}
+AddOnSkinsDS = {
+}
